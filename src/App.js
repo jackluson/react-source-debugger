c2be269f18e8afc872f2762b2683e32398428874
jackluson <luxuemin2108@gmail.com>
2023-04-06 14:51:17 +0800

    chore: eject webpack config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => setCount(count + 1)}>Click me {count}</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
